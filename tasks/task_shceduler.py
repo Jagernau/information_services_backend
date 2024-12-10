39b63989737791dc885bb253aa9b76186e1b07cf
jagernau <stepanenkov25@gmail.com>
2024-12-10 22:17:07 +0300

    refact schedul

--- tasks/task_shceduler.py
+++ tasks/task_shceduler.py
@@ -7,22 +7,31 @@ class Shceduler:
         self.kwargs = kwargs
 
     def _get_interval_convert(self):
+        now = self.kwargs["now_time"]
         if self.kwargs["service_counter"] == 0: # через 5мин
             return {
-                    "start": self.kwargs['now_time'].strftime("%Y-%m-%dT%H:%M:%S"),
-                    "end": (self.kwargs['now_time'] - timedelta(minutes=5)).strftime("%Y-%m-%dT%H:%M:%S")
+                    "start": now.strftime("%Y-%m-%dT%H:%M:%S"),
+                    "end": (now - timedelta(minutes=5)).strftime("%Y-%m-%dT%H:%M:%S")
                     }
         if self.kwargs["service_counter"] == 1: # за предыдущий день
-            now = self.kwargs["now_time"]
             yesterday = now - timedelta(days=1)
             return {
                     "start": yesterday.strftime("%Y-%m-%dT00:01"),
                     "end": yesterday.strftime("%Y-%m-%dT23:59") 
                     }
         if self.kwargs["service_counter"] == 2: # за предыдущюю неделю
-            pass
+            yesterday = now - timedelta(days=1)
+            last_week = yesterday - timedelta(weeks=1)
+            return {
+                "start": last_week.strftime("%Y-%m-%dT00:01"),
+                "end": yesterday.strftime("%Y-%m-%dT23:59")
+            }
         if self.kwargs["service_counter"] == 3: # за предыдущий месяц
-            pass
+            last_month = now.replace(day=1) - timedelta(days=1)
+            return {
+                "start": last_month.replace(day=1).strftime("%Y-%m-%dT00:01"),
+                "end": last_month.strftime("%Y-%m-%dT23:59")
+            }
 
 
         
